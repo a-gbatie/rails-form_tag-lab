35ee42a2e4b38f94bbd35579e52f487e31ac8117
a-gbatie <ange.batie@gmail.com>
2020-12-14 17:31:30 -0800

    submit

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,24 @@
 class StudentsController < ApplicationController
-  
+  before_action :find_student, only: :show
+
   def index
     @students = Student.all
   end
 
   def show
-    @student = Student.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+  @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+  redirect_to student_path(@student)
+  end
+
+  private
+
+  def find_student
+    @student = Student.find(params[:id])
+  end
 end
